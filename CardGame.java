b4c4f8399c9a633878cd36223096b70809a0451f
whm208 <whm208@exeter.ac.uk>
2025-10-14 16:30:36 +0100

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class CardGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number of players:");
-        int playerCount = scanner.nextInt();
-        scanner.nextLine();
+        int playerCount;
+        do {
+            System.out.println("Please enter the number of players:");
+            playerCount = scanner.nextInt();
+            scanner.nextLine();
+            if (playerCount < 1) {
+                System.out.println("There must be at least 1 player.");
+            }
+        } while (playerCount < 1);
         System.out.println("Please enter location of pack to load:");
         String packLocation = scanner.nextLine();
     }
